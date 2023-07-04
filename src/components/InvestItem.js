e10c5807e08f9f56dbad7051db4ae0edf1280ae3
ChenBr <chenbrilling1@gmail.com>
2023-07-04 17:55:39 +0300

    Added currency formatter

--- src/components/InvestItem.js
+++ src/components/InvestItem.js
@@ -1,17 +1,28 @@
 const InvestItem = (props) => {
   let data = props.data;
-
+  const formatter = new Intl.NumberFormat("en-US", {
+    style: "currency",
+    currency: "USD",
+    minimumFractionDigits: 2,
+    maximumFractionDigits: 2,
+  });
   return (
     <tr>
-      <td>{data.year}</td>
-      <td>{data.savingsEndOfYear}</td>
-      <td>{data.yearlyContribution}</td>
+      <td>{formatter.format(data.year)}</td>
+      <td>{formatter.format(data.savingsEndOfYear)}</td>
+      <td>{formatter.format(data.yearlyContribution)}</td>
       <td>
-        {data.savingsEndOfYear -
-          props.currentSavings -
-          data.yearlyContribution * data.year}
+        {formatter.format(
+          data.savingsEndOfYear -
+            props.currentSavings -
+            data.yearlyContribution * data.year,
+        )}
       </td>
-      <td>{props.currentSavings + data.yearlyContribution * data.year}</td>
+      <td>
+        {formatter.format(
+          props.currentSavings + data.yearlyContribution * data.year,
+        )}
+      </td>
     </tr>
   );
 };
